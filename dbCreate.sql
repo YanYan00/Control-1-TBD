3e2f03313d0a5857b68ab249f7944113de3cd065
Jean Rojas <jean.rojas@usach.cl>
2026-04-08 22:45:25 -0400

    Se agregan archivos a trabajar y archivo con link a diagrama lucidchart

---
+++ dbCreate.sql
@@ -0,0 +1,10 @@
+/*
+	Grupo 4 TBD Aerolineas - Creacion BD
+    Integrantes:
+		Jean Rojas
+        Manuel Orellana
+        Manuel Vasquez
+        Luciano Carril
+        Belen Ibañez
+        Vicente Rojas
+*/
---
+++ loadData.sql
@@ -0,0 +1,10 @@
+/*
+	Grupo 4 TBD Aerolineas - Data
+    Integrantes:
+		Jean Rojas
+        Manuel Orellana
+        Manuel Vasquez
+        Luciano Carril
+        Belen Ibañez
+        Vicente Rojas
+*/
