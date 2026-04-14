3e743ec3b8757dcd58bcb2f5ea7e375a5ab196b0
Jean Rojas <jean.rojas@usach.cl>
2026-04-14 16:24:11 -0400

    Agregando tablas a script dbCreate.sql para empezar el desarrollo del control

--- dbCreate.sql
+++ dbCreate.sql
@@ -8,3 +8,38 @@
         Belen Ibañez
         Vicente Rojas
 */
+
+CREATE TABLE client (
+    id_client INT PRIMARY KEY AUTO_INCREMENT,
+    firstname VARCHAR(30),
+    lastname VARCHAR(30),
+    rut VARCHAR(30),
+    nationality VARCHAR(40)
+);
+CREATE TABLE ticket (
+    id_ticket INT PRIMARY KEY AUTO_INCREMENT,
+    date_purchase DATETIME,
+    id_client INT NOT NULL
+);
+CREATE TABLE seat ( 
+    id_seat INT PRIMARY KEY AUTO_INCREMENT,
+    number_seat VARCHAR(3),
+    price INT,
+    section VARCHAR(30),
+    id_ticket INT NOT NULL
+);
+CREATE TABLE company (
+    id_company INT PRIMARY KEY AUTO_INCREMENT,
+    name_company VARCHAR(50),
+    phone VARCHAR(40),
+    email VARCHAR(100),
+    id_flight INT NOT NULL
+);
+CREATE TABLE flight(
+    id_flight INT PRIMARY KEY AUTO_INCREMENT,
+    origin VARCHAR(60),
+    destination VARCHAR(60),
+    flight_state VARCHAR(40),
+    date_flight DATETIME,
+    hour_flight TIME
+);
