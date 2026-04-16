b1408ff99372790f6d703dd4ee8286335892295b
Beldr4k4r <belen.ibanez@usach.cl>
2026-04-16 14:05:23 -0400

    Minor errors fixed in queries

--- runStatements.sql
+++ runStatements.sql
@@ -12,20 +12,20 @@ USE airport;
 -- query 1
 SELECT f.destination, COUNT(*) AS total_viajes
 FROM ticket t
-join clients c on t.id_client = c.id_client
-join flight f on f.id_flight = t.id_flight
-WHERE c.nationality = 'Chile' AND f.date_flight >= now() - interval 4 year
-GROUP BY f.Destination
-ORDER By total_viajes DESC;
+JOIN client c ON t.id_client = c.id_client
+JOIN flight f ON f.id_flight = t.id_flight
+WHERE c.nationality = 'Chile' 
+  AND f.date_flight >= NOW() - INTERVAL 4 YEAR
+GROUP BY f.destination
+ORDER BY total_viajes DESC;
 
 
 -- query 2 
 -- listar las secciones mas compradas por argentinos
-
-SELECT s.seccion, count(*) AS total_compras 
+SELECT s.section, COUNT(*) AS total_compras 
 FROM ticket t 
-JOIN clients c on c.id_client = t.id_client
-JOIN seat s on s.id_ticket=t.id_ticket
-where c.nationality = "Argentino"
+JOIN client c ON c.id_client = t.id_client
+JOIN seat s ON s.id_ticket = t.id_ticket
+WHERE c.nationality = "Argentina"
 GROUP BY s.section
-ORDER BY total_compras desc;
+ORDER BY total_compras DESC;
