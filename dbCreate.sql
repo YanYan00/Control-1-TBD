e74b8d6aab23ad148f835643824eedb67ce60115
Manuel Orellana <manuel.orellana.s@usach.cl>
2026-04-17 21:25:45 -0400

    Traduction to postgres of the data base, the data and the query's, and add 1 query

--- dbCreate.sql
+++ dbCreate.sql
@@ -8,13 +8,8 @@
         Belen Ibañez
         Vicente Rojas
 */
-DROP DATABASE IF EXISTS airport;
-CREATE DATABASE IF NOT EXISTS airport;
-USE airport;
--- Tables without dependency of other
-
 CREATE TABLE clients (
-    id_client INT PRIMARY KEY AUTO_INCREMENT,
+    id_client SERIAL PRIMARY KEY,
     firstname VARCHAR(30),
     lastname VARCHAR(30),
     passport_number VARCHAR(40),
@@ -22,66 +17,66 @@ CREATE TABLE clients (
 );
 
 CREATE TABLE company (
-    id_company INT PRIMARY KEY AUTO_INCREMENT,
+    id_company SERIAL PRIMARY KEY,
     name_company VARCHAR(50),
     phone VARCHAR(40),
     email VARCHAR(100)
 );
 
--- Tables with dependency of other
-CREATE TABLE  employee(
-	id_employee INT PRIMARY KEY AUTO_INCREMENT,
+-- Tables with dependency
+CREATE TABLE employee (
+    id_employee SERIAL PRIMARY KEY,
     firstname VARCHAR(50),
     lastname VARCHAR(50),
     salary INT,
     title VARCHAR(30),
     id_company INT,
-    FOREIGN KEY (id_company) REFERENCES company(id_company) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT fk_company FOREIGN KEY (id_company) REFERENCES company(id_company) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE plane(
-	id_plane INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE plane (
+    id_plane SERIAL PRIMARY KEY,
     model VARCHAR(30),
-    last_maintenance DATETIME,
+    last_maintenance TIMESTAMP,
     id_company INT NOT NULL,
-    FOREIGN KEY (id_company) REFERENCES company(id_company) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT fk_company_plane FOREIGN KEY (id_company) REFERENCES company(id_company) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-
-CREATE TABLE flight(
-    id_flight INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE flight (
+    id_flight SERIAL PRIMARY KEY,
     origin VARCHAR(60),
     destination VARCHAR(60),
     flight_state VARCHAR(40),
-    date_flight DATETIME,
+    date_flight TIMESTAMP,
     hour_flight TIME,
     id_plane INT,
-    FOREIGN KEY (id_plane) REFERENCES plane(id_plane) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT fk_plane FOREIGN KEY (id_plane) REFERENCES plane(id_plane) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE ticket (
-    id_ticket INT PRIMARY KEY AUTO_INCREMENT,
-    date_purchase DATETIME,
+    id_ticket SERIAL PRIMARY KEY,
+    date_purchase TIMESTAMP,
     id_client INT NOT NULL,
     id_flight INT NOT NULL,
-    FOREIGN KEY (id_client) REFERENCES clients(id_client) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (id_flight) REFERENCES flight(id_flight) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT fk_client FOREIGN KEY (id_client) REFERENCES clients(id_client) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_flight FOREIGN KEY (id_flight) REFERENCES flight(id_flight) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE flight_employee(
-	id_employee INT NOT NULL,
-    FOREIGN KEY (id_employee) REFERENCES employee(id_employee) ON DELETE CASCADE ON UPDATE CASCADE,
+CREATE TABLE flight_employee (
+    id_employee INT NOT NULL,
     id_flight INT NOT NULL,
-    FOREIGN KEY (id_flight) REFERENCES flight(id_flight) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT fk_employee FOREIGN KEY (id_employee) REFERENCES employee(id_employee) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_flight_emp FOREIGN KEY (id_flight) REFERENCES flight(id_flight) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (id_employee, id_flight) -- Se agrega una llave primaria compuesta por buena práctica
 );
 
 CREATE TABLE seat ( 
-    id_seat INT PRIMARY KEY AUTO_INCREMENT,
+    id_seat SERIAL PRIMARY KEY,
     number_seat VARCHAR(3),
     price INT,
     section VARCHAR(30),
     id_ticket INT NOT NULL,
-    FOREIGN KEY (id_ticket) REFERENCES ticket(id_ticket) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT fk_ticket FOREIGN KEY (id_ticket) REFERENCES ticket(id_ticket) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
--- runStatements.sql
+++ runStatements.sql
@@ -8,14 +8,13 @@
         Belen Ibañez
         Vicente Rojas
 */
-USE airport;
 -- query 1
 SELECT f.destination, COUNT(*) AS total_viajes
 FROM ticket t
 JOIN clients c ON t.id_client = c.id_client
 JOIN flight f ON f.id_flight = t.id_flight
 WHERE c.nationality = 'Chile' 
-  AND f.date_flight >= NOW() - INTERVAL 4 YEAR
+  AND f.date_flight >= NOW() - INTERVAL '4 years'
 GROUP BY f.destination
 ORDER BY total_viajes DESC;
 
@@ -26,7 +25,7 @@ SELECT s.section, COUNT(*) AS total_compras
 FROM ticket t 
 JOIN clients c ON c.id_client = t.id_client
 JOIN seat s ON s.id_ticket = t.id_ticket
-WHERE c.nationality = "Argentina"
+WHERE c.nationality = 'Argentina'
 GROUP BY s.section
 ORDER BY total_compras DESC;
 
@@ -35,15 +34,15 @@ ORDER BY total_compras DESC;
 SELECT mes, nationality, total_personas
 FROM (
     SELECT 
-        MONTH(f.date_flight) AS mes, 
+        EXTRACT(MONTH FROM f.date_flight) AS mes, 
         c.nationality, 
         COUNT(*) AS total_personas,
-        ROW_NUMBER() OVER(PARTITION BY MONTH(f.date_flight) ORDER BY COUNT(*) DESC) AS ranking -- por cada mes se hace un ranking del total de personas
+        ROW_NUMBER() OVER(PARTITION BY EXTRACT(MONTH FROM f.date_flight) ORDER BY COUNT(*) DESC) AS ranking -- por cada mes se hace un ranking del total de personas
     FROM ticket t
     JOIN clients c ON t.id_client = c.id_client
     JOIN flight f ON f.id_flight = t.id_flight -- Se crea la tabla que relaciona pasajero con vuelo
-    WHERE f.date_flight >= DATE_SUB(CURDATE(), INTERVAL 4 YEAR) -- filtro 4 años
-      AND f.date_flight <= CURDATE()
+    WHERE f.date_flight >= CURRENT_DATE - INTERVAL '4 years'  -- filtro 4 años
+      AND f.date_flight <= CURRENT_DATE
     GROUP BY mes, c.nationality -- Se ordena esta tabla de pasajero vuelo por pais
 ) AS tabla_ranking
 WHERE ranking = 1
@@ -54,12 +53,21 @@ ORDER BY mes; -- sacame solo los ranking 1 ordenados por mes
 SELECT 
     c.firstname, 
     c.lastname, 
-    MONTH(t.date_purchase) AS mes, 
+    EXTRACT(MONTH FROM t.date_purchase) AS mes, 
     COUNT(*) AS cantidad_vuelos
 FROM ticket t
 JOIN clients c ON t.id_client = c.id_client
 JOIN seat s ON t.id_ticket = s.id_ticket  -- Tabla union persona asiento por ticket
 WHERE s.section = 'First Class' -- todos los primera lase
 GROUP BY mes, c.id_client, c.firstname, c.lastname -- se ordena por mes  y persona
-HAVING cantidad_vuelos > 4 -- que tenga mas de 4 vuelos
+HAVING COUNT(*) > 4 -- que tenga mas de 4 vuelos
 ORDER BY mes ASC, cantidad_vuelos DESC;
+
+-- query 5
+SELECT p.id_plane, COUNT(f.id_flight) AS total_aviones
+FROM plane p
+LEFT JOIN flight f ON p.id_plane = f.id_plane
+GROUP BY p.id_plane
+ORDER BY total_aviones ASC
+LIMIT 1;
+
